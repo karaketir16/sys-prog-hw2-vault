81b7ed6504e6a8fb72733d6663776b6ad2b230e9
Osman Karaketir <osmankaraketir@gmail.com>
2020-12-26 17:54:33 +0300

    fix typo

--- vector.c
+++ vector.c
@@ -15,18 +15,18 @@
 
 #include <stdio.h>
 int main(){
-    char_vector chv = CR_create();
+    char_vector chv = CV_create();
     for(char c = 'a'; c <='z'; c++){
-        CR_push(&chv, c);
+        CV_push(&chv, c);
     }
-    char_vector_2D chvv = CR2D_create();
+    char_vector_2D chvv = CV2D_create();
     for(char c = 'a'; c <='z'; c++){
-        CR2D_push(&chvv, chv);
+        CV2D_push(&chvv, chv);
     }
     for(char i = 0; 'a' + i <='z'; i++){
-        char_vector tmp = CR2D_pop(&chvv);
+        char_vector tmp = CV2D_pop(&chvv);
         for(char j = 0; 'a' + j <='z'; j++){
-            printf("test: %c\n", CR_pop(&tmp));
+            printf("test: %c\n", CV_pop(&tmp));
         }
     }
     return 0;
@@ -36,7 +36,7 @@ int main(){
 
 
 
-char_vector CR_create(){
+char_vector CV_create(){
     char_vector tmp;
     tmp.size = 0;
     tmp.cap = tmp.size + 1;
@@ -44,16 +44,16 @@ char_vector CR_create(){
     return tmp;
 }
 
-char CR_set_index(char_vector *this, int index, char ch){
+char CV_set_index(char_vector *this, int index, char ch){
     return this->data[index] = ch;
 }
 
-char CR_get_index(char_vector *this, int index){
+char CV_get_index(char_vector *this, int index){
     return this->data[index];
 }
 
-char CR_push(char_vector *this, char ch){
-    CR_set_index(this, this->size, ch);
+char CV_push(char_vector *this, char ch){
+    CV_set_index(this, this->size, ch);
     this->size++;
     if(this->size == this->cap){
         char *tmp = (char*) kmalloc(this->cap * 2 * sizeof(char), GFP_KERNEL);
@@ -65,12 +65,12 @@ char CR_push(char_vector *this, char ch){
     return ch;
 }
 
-char CR_pop(char_vector *this){
+char CV_pop(char_vector *this){
     this->size--;
-    return CR_get_index(this, this->size);
+    return CV_get_index(this, this->size);
 }
 
-char_vector_2D CR2D_create(){
+char_vector_2D CV2D_create(){
     char_vector_2D tmp;
     tmp.size = 0;
     tmp.cap = tmp.size + 1;
@@ -78,15 +78,15 @@ char_vector_2D CR2D_create(){
     return tmp;
 }
 
-char_vector CR2D_set_index(char_vector_2D *this, int index, char_vector chv){
+char_vector CV2D_set_index(char_vector_2D *this, int index, char_vector chv){
     return this->data[index] = chv;
 }
-char_vector CR2D_get_index(char_vector_2D *this, int index){
+char_vector CV2D_get_index(char_vector_2D *this, int index){
     return this->data[index];
 }
 
-char_vector CR2D_push(char_vector_2D *this, char_vector chv){
-    CR2D_set_index(this, this->size, chv);
+char_vector CV2D_push(char_vector_2D *this, char_vector chv){
+    CV2D_set_index(this, this->size, chv);
     this->size++;
     if(this->size == this->cap){
         char_vector *tmp = (char_vector*) kmalloc(this->cap * 2 * sizeof(char_vector), GFP_KERNEL);
@@ -97,17 +97,17 @@ char_vector CR2D_push(char_vector_2D *this, char_vector chv){
     }
     return chv;
 }
-char_vector CR2D_pop(char_vector_2D *this){
+char_vector CV2D_pop(char_vector_2D *this){
     this->size--;
-    return CR2D_get_index(this, this->size);
+    return CV2D_get_index(this, this->size);
 }
 
-char CR2D_set(char_vector_2D *this, int index_i, int index_j, char ch){
-    char_vector tmp = CR2D_get_index(this, index_i);
-    return CR_set_index(&tmp, index_j, ch);
+char CV2D_set(char_vector_2D *this, int index_i, int index_j, char ch){
+    char_vector tmp = CV2D_get_index(this, index_i);
+    return CV_set_index(&tmp, index_j, ch);
 }
 
-char CR2D_get(char_vector_2D *this, int index_i, int index_j){
-    char_vector tmp = CR2D_get_index(this, index_i);
-    return CR_get_index(&tmp, index_j);
+char CV2D_get(char_vector_2D *this, int index_i, int index_j){
+    char_vector tmp = CV2D_get_index(this, index_i);
+    return CV_get_index(&tmp, index_j);
 }
--- vector.h
+++ vector.h
@@ -8,11 +8,11 @@ typedef struct char_vector
     int cap;
 } char_vector;
 
-char_vector CR_create(void);
-char CR_push(char_vector *this, char ch);
-char CR_pop(char_vector *this);
-char CR_set_index(char_vector *this, int index, char ch);
-char CR_get_index(char_vector *this, int index);
+char_vector CV_create(void);
+char CV_push(char_vector *this, char ch);
+char CV_pop(char_vector *this);
+char CV_set_index(char_vector *this, int index, char ch);
+char CV_get_index(char_vector *this, int index);
 
 typedef struct char_vector_2D
 {
@@ -22,13 +22,13 @@ typedef struct char_vector_2D
 } char_vector_2D;
 
 
-char_vector_2D CR2D_create(void);
-char_vector CR2D_push(char_vector_2D *this, char_vector chv);
-char_vector CR2D_pop(char_vector_2D *this);
-char_vector CR2D_set_index(char_vector_2D *this, int index, char_vector chv);
-char_vector CR2D_get_index(char_vector_2D *this, int index);
+char_vector_2D CV2D_create(void);
+char_vector CV2D_push(char_vector_2D *this, char_vector chv);
+char_vector CV2D_pop(char_vector_2D *this);
+char_vector CV2D_set_index(char_vector_2D *this, int index, char_vector chv);
+char_vector CV2D_get_index(char_vector_2D *this, int index);
 
-char CR2D_set(char_vector_2D *this, int index_i, int index_j, char ch);
-char CR2D_get(char_vector_2D *this, int index_i, int index_j);
+char CV2D_set(char_vector_2D *this, int index_i, int index_j, char ch);
+char CV2D_get(char_vector_2D *this, int index_i, int index_j);
 
 #endif //OUR_VECTOR
